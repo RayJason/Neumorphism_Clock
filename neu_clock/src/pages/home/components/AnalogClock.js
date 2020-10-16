99a2b570fb05f695703b4d4ed49a975b95e1c8ec
RayJason <rayjason@stu.jluzh.edu.cn>
2020-10-16 14:14:19 +0800

    - 数字时钟、模拟时钟转为函数组件

--- neu_clock/src/pages/home/components/AnalogClock.js
+++ neu_clock/src/pages/home/components/AnalogClock.js
@@ -1,125 +1,73 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styles from './AnalogClock.module.scss'
 
-export default class AnalogClock extends Component {
-  constructor() {
-    super()
-    this.state = {
-      date: '', // 时钟下面显示的时间
-      settedTime: '0:0:0', // 正在运行的初始时间
-      prevTimestamp: 0, // 设置自定义时间时的时间戳
+function AnalogClock(props) {
+  let hourRotate = 0 // 时针角度
+  let minuteRotate = 0 // 分针角度
+  let secondRotate = 0 // 秒针角度
 
-      hourRotate: 0, // 时针角度
-      minuteRotate: 0, // 分针角度
-      secondRotate: 0, // 秒针角度
+  let time = '' // 数字时钟
 
-      renderClockTimeout: '', // 更新时钟计时器
-    }
-  }
-
-  // 在第一次渲染后调用
-  componentDidMount() {
-    this.renderClock()
-  }
-
-  componentDidUpdate() {
-    // console.log('componentDidUpdate的props.Time', this.props.Time)
-    console.log('componentDidUpdate的settedTime', this.state.settedTime)
-    if (this.props.Time && this.props.Time !== this.state.settedTime) {
-      this.setState(
-        {
-          settedTime: this.props.Time,
-          prevTimestamp: this.props.timestamp, // 单位：秒
-        },
-        () => this.renderClock()
-      )
-    }
-  }
-
-  // 在组件从 DOM 中移除之前立刻被调用
-  componentWillUnmount() {
-    clearTimeout(this.state.renderClockTimeout)
-  }
-
-  // 更新时钟
-  renderClock = () => {
-    // 清除计时器
-    clearTimeout(this.state.renderClockTimeout)
-    let data = this.state.settedTime
+  function renderClock() {
+    let data = props.Time
     let hour = Number(data.split(':')[0])
     let minute = Number(data.split(':')[1])
     let second = Number(data.split(':')[2])
 
-    let nowTimestamp = Math.round(new Date() / 1000) + 28800000 // 当前时间戳(+8小时转换到东八区北京时间)
-    let outTime = nowTimestamp - this.state.prevTimestamp // 相对时差
+    hourRotate = (hour % 12) * 30 + minute / 2
+    minuteRotate = minute * 6 + second / 10
+    secondRotate = second * 6
 
-    let allSecond = second + outTime // 秒
-    let allMinute = minute * 60 + allSecond // 截止到分的总秒
-
-    second = allSecond % 60
-    minute = (minute + Math.floor(allSecond / 60)) % 60
-    hour = (hour + Math.floor(allMinute / 3600)) % 24
-
-    let time =
+    // 显示的时间
+    time =
       (hour < 10 ? '0' + hour : hour) +
       ':' +
       (minute < 10 ? '0' + minute : minute) +
       ':' +
       (second < 10 ? '0' + second : second)
-
-    this.setState({
-      date: time,
-      hourRotate: (hour % 12) * 30 + minute / 2,
-      minuteRotate: minute * 6 + second / 10,
-      secondRotate: second * 6,
-      renderClockTimeout: setTimeout(this.renderClock, 1000),
-    })
   }
 
-  render() {
-    return (
-      <div className={`columnCenter`}>
+  renderClock()
+
+  return (
+    <div className={`columnCenter`}>
+      <div
+        className={`neuShadow_border_clock rowCenter ${styles.AnalogClock} ${styles.lineTransition}`}
+        style={{
+          width: props.Width,
+          height: props.Height,
+        }}
+      >
+        {/* 时针 */}
         <div
-          className={`neuShadow_border_clock rowCenter ${styles.AnalogClock} ${styles.lineTransition}`}
+          className={`${styles.hourLine} ${styles.lineTransition}`}
           style={{
-            width: this.props.Width,
-            height: this.props.Height,
+            transform: 'translateY(-50%) rotateZ(' + hourRotate + 'deg)',
           }}
-        >
-          {/* 时针 */}
-          <div
-            className={`${styles.hourLine} ${styles.lineTransition}`}
-            style={{
-              transform:
-                'translateY(-50%) rotateZ(' + this.state.hourRotate + 'deg)',
-            }}
-          ></div>
+        ></div>
 
-          {/* 秒针 */}
-          <div
-            className={`${styles.secondLine} ${styles.lineTransition} `}
-            style={{
-              transform:
-                'translateY(-50%) rotateZ(' + this.state.secondRotate + 'deg)',
-            }}
-          ></div>
-          {/* 分针 */}
-          <div
-            className={`${styles.minuteLine} ${styles.lineTransition} `}
-            style={{
-              transform:
-                'translateY(-50%) rotateZ(' + this.state.minuteRotate + 'deg)',
-            }}
-          ></div>
+        {/* 秒针 */}
+        <div
+          className={`${styles.secondLine} ${styles.lineTransition} `}
+          style={{
+            transform: 'translateY(-50%) rotateZ(' + secondRotate + 'deg)',
+          }}
+        ></div>
+        {/* 分针 */}
+        <div
+          className={`${styles.minuteLine} ${styles.lineTransition} `}
+          style={{
+            transform: 'translateY(-50%) rotateZ(' + minuteRotate + 'deg)',
+          }}
+        ></div>
 
-          <div className={styles.dot}></div>
-        </div>
-        <div className={styles.time}>
-          <div>{this.state.date}</div>
-        </div>
+        <div className={styles.dot}></div>
       </div>
-    )
-  }
+      <div className={styles.time}>
+        <div>{time}</div>
+      </div>
+    </div>
+  )
 }
 
 // 默认props
@@ -127,3 +75,5 @@ AnalogClock.defaultProps = {
   Width: '30vw',
   Height: '30vw',
 }
+
+export default AnalogClock
--- neu_clock/src/pages/home/components/clock.js
+++ neu_clock/src/pages/home/components/clock.js
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React, { Component } from 'react'
 import AnalogClock from './AnalogClock'
 import DigitalClock from './DigitalClock'
 
@@ -11,62 +11,14 @@ import DigitalClock from './DigitalClock'
     Time: 自定义时间
 */
 
-// function clock(props) {
-//   // 计算显示的时间
-//   function calcuTime() {
-//     let settedTime = '0:0:0' // 自定义的时间 格式 h:m:S
-//     settedTime = props.Time ? props.Time : settedTime
-//     console.log('传入的自定义时间：', props.Time)
-
-//     // let updataTimeout = '' // 更新时钟计时器
-
-//     let hour = Number(settedTime.split(':')[0])
-//     let minute = Number(settedTime.split(':')[1])
-//     let second = Number(settedTime.split(':')[2])
-
-//     let nowTimestamp = Math.round(new Date() / 1000) + 28800000 // 当前时间戳(+8小时转换到东八区北京时间)
-//     let outTime = nowTimestamp - props.timestamp // 相对时差
-
-//     let allSecond = second + outTime // 秒
-//     let allMinute = minute * 60 + allSecond // 截止到分的总秒
-
-//     second = allSecond % 60
-//     minute = (minute + Math.floor(allSecond / 60)) % 60
-//     hour = (hour + Math.floor(allMinute / 3600)) % 24
-
-//     return hour + ':' + minute + ':' + second
-//   }
-
-//   // 选择时钟风格
-//   function renderClock() {
-//     console.log(calcuTime())
-//     switch (props.clockStyle) {
-//       case 0:
-//         // 数字时钟
-//         return <DigitalClock Time={calcuTime()} timestamp={props.timestamp} />
-//       case 1:
-//         // 模拟时钟
-//         return <AnalogClock Time={calcuTime()} timestamp={props.timestamp} />
-
-//       default:
-//         // 默认模拟时钟
-//         return <AnalogClock Time={calcuTime()} timestamp={props.timestamp} />
-//     }
-//   }
-
-//   return renderClock()
-// }
-
-// export default clock
-
-import React, { Component } from 'react'
-
 export default class clock extends Component {
   constructor() {
     super()
     this.state = {
       settedTime: '0:0:0', // 自定义的时间
       nowTime: '0:0:0', // 现在显示的时间
+
+      renderClockTimeout: '', // 更新时钟计时器
     }
   }
 
@@ -89,17 +41,21 @@ export default class clock extends Component {
     }
   }
 
+  // 在组件从 DOM 中移除之前立刻被调用
+  componentWillUnmount() {
+    clearTimeout(this.state.renderClockTimeout)
+  }
+
   // 计算显示的时间
   calcuTime = () => {
-    console.log('传入的自定义时间：', this.props.Time)
+    // console.log('传入的自定义时间：', this.props.Time)
     let settedTime = this.state.settedTime
-
-    // let updataTimeout = '' // 更新时钟计时器
 
     let hour = Number(settedTime.split(':')[0])
     let minute = Number(settedTime.split(':')[1])
     let second = Number(settedTime.split(':')[2])
 
+    // 计算现在相对的时间
     let nowTimestamp = Math.round(new Date() / 1000) + 28800000 // 当前时间戳(+8小时转换到东八区北京时间)
     let outTime = nowTimestamp - this.props.timestamp // 相对时差
 
@@ -112,12 +68,12 @@ export default class clock extends Component {
 
     this.setState({
       nowTime: hour + ':' + minute + ':' + second,
+      renderClockTimeout: setTimeout(this.calcuTime, 1000),
     })
   }
 
   // 选择时钟风格
   RenderClock = () => {
-    console.log(this.state.nowTime)
     switch (this.props.clockStyle) {
       case 0:
         // 数字时钟
@@ -135,7 +91,6 @@ export default class clock extends Component {
             timestamp={this.props.timestamp}
           />
         )
-
       default:
         // 默认模拟时钟
         return (
@@ -146,6 +101,7 @@ export default class clock extends Component {
         )
     }
   }
+  
   render() {
     return <this.RenderClock />
   }
